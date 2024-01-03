adc0b24f8ac95b9dc9bcef06ed16168e218a67f3
Pavel Shlapak <letohx@gmail.com>
2024-01-03 03:41:36 +0300

    feat: add solution

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -397,8 +397,25 @@ function sortByAsc(/* arr */) {
  *  '012345', 3 => '024135' => '043215' => '031425'
  *  'qwerty', 3 => 'qetwry' => 'qtrewy' => 'qrwtey'
  */
-function shuffleChar(/* str, iterations */) {
-  throw new Error('Not implemented');
+function shuffleChar(str, iterations) {
+  let result = str;
+
+  for (let i = 0; i < iterations; i += 1) {
+    let start = '';
+    let end = '';
+
+    for (let j = 0; j < str.length; j += 1) {
+      if (j % 2) {
+        end += result[j];
+      } else {
+        start += result[j];
+      }
+    }
+
+    result = `${start}${end}`;
+  }
+
+  return result;
 }
 
 /**
